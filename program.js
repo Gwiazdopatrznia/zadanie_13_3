c909ebb632df943801135b5f00be2ccaaad1f98c
Anna Gajewska <gajewska.anna@o2.pl>
2018-05-12 19:25:45 +0200

    Change in program.js

--- program.js
+++ program.js
@@ -1,21 +1,23 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
+
+    if (!input) return;
     if (input !== null) {
         var instruction = input.toString().trim();
     }
     switch (instruction) {
-    	case 'version':
-        	console.log('Node version: ' + process.versions.node);
-        	break;
+        case 'version':
+            console.log('Node version: ' + process.versions.node);
+            break;
         case 'lang':
-        	console.log('Lang: ' + process.env.LANG);
-        	break;
+            console.log('Lang: ' + process.env.LANG);
+            break;
         case 'exit':
-        	process.stdout.write('Quitting app!\n');
-        	process.exit();
-        	break;
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
         default:
-        	process.stderr.write('Enter "version", "lang", "exit".\n');
+            process.stderr.write('Enter "version", "lang", "exit".\n');
     }
 });

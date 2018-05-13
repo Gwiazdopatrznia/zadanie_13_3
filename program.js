bf72cc38d3aef929377e43c51598f825e5c52d59
Anna Gajewska <gajewska.anna@o2.pl>
2018-05-13 23:55:02 +0200

    Change in program.js

--- program.js
+++ program.js
@@ -3,9 +3,9 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
 
     if (!input) return;
-    if (input !== null) {
-        var instruction = input.toString().trim();
-    }
+    
+    var instruction = input.toString().trim();
+    
     switch (instruction) {
         case 'version':
             console.log('Node version: ' + process.versions.node);
